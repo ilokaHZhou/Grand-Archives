b1e47dbc2ca827384e65b942cb51e0cea542ca1d
周恒 <15129463+zhou-harry23@user.noreply.gitee.com>
2024-12-09 23:54:36 +0800

    minor change

--- docs/Algorithm/Sorting Algorithm/Sort.java
+++ docs/Algorithm/Sorting Algorithm/Sort.java
@@ -5,18 +5,59 @@ public class Sort {
         }
     }
 
+    public static int[] bubbleSort(int[] array) {
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void quickSort2(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = partition(arr, left, right);
+            quickSort2(arr, left, pivot - 1);
+            quickSort2(arr, pivot + 1, right);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int mark = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (arr[i] < pivot) {
+                mark++;
+                int temp = arr[mark];
+                arr[mark] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        arr[left] = arr[mark];
+        arr[mark] = pivot;
+        return mark;
+    }
+
 
     public static void main(String[] args) {
-        int[] array = {10, 7, 8, 9, 1, 5};
+        int[] array = {10, 7, 8, 9, 1, 5, 3, 2};
         System.out.println("Original Array:" );
         for (int num : array) {
             System.out.print(num + " ");
         }
 
-        quickSort(array, 0, array.length - 1);
+        int[] result = bubbleSort(array);
+
+        // quickSort(array, 0, array.length - 1);
+        quickSort2(array, 0, array.length - 1);
 
         System.out.println("\nSorted Array:" );
-        for (int num : array) {
+        for (int num : result) {
             System.out.print(num + " ");
         }
     }
