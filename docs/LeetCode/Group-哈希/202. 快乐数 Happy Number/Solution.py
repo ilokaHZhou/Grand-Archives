7080953cab49e8a0d19be88c93643fd4bd9d0d23
周恒 <15129463+zhou-harry23@user.noreply.gitee.com>
2025-01-05 09:23:17 +0800

    add more hashes

--- docs/LeetCode/Group-哈希/202. 快乐数 Happy Number/Solution.py
+++ docs/LeetCode/Group-哈希/202. 快乐数 Happy Number/Solution.py
@@ -1,14 +1,14 @@
+# 由于可以无限循环，用set来记录每个变换后的数字
+# 不是快乐数的变着变着就会重复自身陷入循环，始终变不到1，快乐数的最终会变成1
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # 由于可以无限循环，有些数变着变着就会重复自身陷入循环，始终变不到1
-        # 所以用set来记录
         record = set()
         while n not in record:
             record.add(n)
             new_num = 0
             n_str = str(n)
             for i in n_str:
-                new_num = int(i) ** 2
+                new_num = new_num + int(i) ** 2
             if new_num == 1:
                 return True
             else:
