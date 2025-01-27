afc7f26d221cffc0f1f6ebfa4d3d93325f8dcac9
周恒 <hengzhou23@163.com>
2025-01-27 22:00:22 +0800

    add more pages

--- docs/LeetCode/1047. 删除字符串中的所有相邻重复项 Remove All Adjacent Duplicates In String/Solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, s: str) -> str:
-        res = list()
-        for item in s:
-            if res and res[-1] == item: # 检查是否要移除栈顶元素
-                res.pop()
-            else:
-                res.append(item)
-        return ''.join(res)
--- docs/LeetCode/Group-数组/1431. 拥有最多糖果的孩子 Kids With The Greatest Number Of Candies/Solution.py
+++
@@ -1,6 +0,0 @@
-# 先找最大值再比对即可
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        maxCandies = max(candies)
-        result = [candy + extraCandies >= maxCandies for candy in candies]
-        return result
